d1cdc5d91cef677b6e68622d96928bd7882e3503
veloadmingitea <senecjusz@gmail.com>
2025-10-21 13:08:38 +0000

    Update userscripts/mark-reviewed-documents.user.js

--- userscripts/mark-reviewed-documents.user.js
+++ userscripts/mark-reviewed-documents.user.js
@@ -7,8 +7,9 @@
 // @run-at       document-idle
 // @grant        GM_addStyle
 // @grant        GM_registerMenuCommand
-// @updateURL    https://git.ts.archax.eu/veloadmingitea/chudyyFix/raw/branch/main/userscripts/mark-reviewed-documents.user.js
-// @downloadURL  https://git.ts.archax.eu/veloadmingitea/chudyyFix/raw/branch/main/userscripts/mark-reviewed-documents.user.js
+// @updateURL   https://raw.githubusercontent.com/senecjusz/chudyyFix/main/userscripts/mark-reviewed-documents.user.js
+// @downloadURL https://raw.githubusercontent.com/senecjusz/chudyyFix/main/userscripts/mark-reviewed-documents.user.js
+
 // ==/UserScript==
 
 (async () => {
